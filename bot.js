683d70dabe507a913aed60fac32846f741d5840b
jovid1242 <jovid1242@gmail.com>
2021-03-12 21:58:36 +0500

    fix bot.js

--- bot.js
+++ bot.js
@@ -12,20 +12,9 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const menuMiddleware = new MenuMiddleware('/', menuTemplate)
 
-bot.start(async (ctx) => {
-    const check = await checkUser(ctx.message.chat.id)
-    if (check === null) { menuMiddleware.replyToContext(ctx) } else {
-        if (check === true) { ctx.reply('Привет Админ отправьте мне код купона!') } else {
-            ctx.reply(`Добро пожаловать ${ctx.from.first_name}, Ваш код купона ${check.coupon}`, Extra.markup((markup) => {
-                return markup.resize().keyboard([(`Статус`), (`Вывод денег`)])
-            }))
-        }
-    }
-})
-
+bot.start(async (ctx) => { await checkUser(ctx.message.chat.id) })
 bot.on('message', async (ctx) => { await checkMessage(ctx) })
 bot.help((ctx) => ctx.reply(`dos't held  `))
-// bot.use(Telegraf.log())
 bot.use(menuMiddleware)
 bot.launch()
 
--- modules/checkuser.js
+++ modules/checkuser.js
@@ -3,9 +3,17 @@ const db = require('../db/config.js')
 const checkUser = function (chat_id) {
     const collection = global.DB.collection("tg_users")
     return collection.findOne({ chat_id: chat_id }).then((user => {
-        if (user.admin === 1) {
-            return true
-        } else { return user }
+        if (user === null) {
+            return menuMiddleware.replyToContext(ctx)
+        } else {
+            if (user.admin === 1) {
+                return ctx.reply('Привет Админ отправьте мне код купона!')
+            } else {
+                ctx.reply(`Добро пожаловать ${ctx.from.first_name}, Ваш код купона ${check.coupon}`, Extra.markup((markup) => {
+                    return markup.resize().keyboard([(`Статус`), (`Вывод денег`)])
+                }))
+            }
+        }
     }))
 }
 
